5875d7b9f7caf61fd0893e7b955a04724cb82788
shankar <shankarlal-shyamlal.gupta@capgemini.com>
2019-01-22 08:42:33 +0530

    interface must be initialized

--- Java_Practice/src/com/interfaces/MyInterface.java
+++ Java_Practice/src/com/interfaces/MyInterface.java
@@ -12,6 +12,8 @@ public interface MyInterface {
 	String str = "My Interface";
 	public static final int a = 5;
 
+	//int c;    // interface variable must be initialized in the interface otherwaise it's  throws an error
+
 	void method1();
 
 	public abstract int method(int a); // checking interface contain return values or not
