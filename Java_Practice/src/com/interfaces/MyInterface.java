3b5b0a1f729074791129773b1cfc098303a73cd1
shankar <shankarlal-shyamlal.gupta@capgemini.com>
2019-01-22 08:35:12 +0530

    Interface example

---
+++ Java_Practice/src/com/interfaces/MyInterface.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.interfaces;
+
+/**
+ * @author Shankar
+ *
+ */
+public interface MyInterface {
+
+	String str = "My Interface";
+	public static final int a = 5;
+
+	void method1();
+
+	public abstract int method(int a); // checking interface contain return values or not
+
+}
